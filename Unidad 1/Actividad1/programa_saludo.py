3f41592666f0425a5e49812514372dc9b3366215
Eunice <davilaeunice3@gmail.com>
2025-09-07 15:52:26 -0500

    Parte A - Programas en Python

---
+++ Unidad 1/Actividad1/programa_saludo.py
@@ -0,0 +1,8 @@
+# a) Mostrar un saludo
+print("Hola, este es mi primer programa en Python")
+
+# b) Solicitar nombre y edad
+nombre = input("¿Cuál es tu nombre? ")
+edad = input("¿Cuántos años tienes? ")
+
+print(f"Hola {nombre}, tienes {edad} años")
