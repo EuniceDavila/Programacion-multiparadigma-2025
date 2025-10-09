755b937e7fdd53798adb92d80a4c9a3a384f75dc
Eunice <davilaeunice3@gmail.com>
2025-10-08 19:55:25 -0500

    Clase libro modificada, cambio de carpeta

---
+++ Unidad2/ClaseSimple/libro.py
@@ -0,0 +1,44 @@
+class Libro:
+    # Atributo de clase
+    biblioteca = "Biblioteca Central"
+
+    # Constructor (__init__)
+    def __init__(self, titulo, autor, anio_publicacion):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio_publicacion = anio_publicacion
+        self.prestado = False  # Inicialmente no está prestado
+
+    # Método para prestar el libro
+    def prestar(self):
+        if not self.prestado:
+            self.prestado = True
+            
+            print(f"El libro '{self.titulo}' ha sido prestado.")
+            print("------------------------------------------------------------" )
+        else:
+           
+            print(f"El libro '{self.titulo}' ya está prestado.")
+            print("------------------------------------------------------------" )
+
+    # Método para devolver el libro
+    def devolver(self):
+        if self.prestado:
+            self.prestado = False
+            
+            print(f"El libro '{self.titulo}' ha sido devuelto.")
+            print("------------------------------------------------------------" )
+        else:
+         
+            print(f"El libro '{self.titulo}' no estaba prestado.")
+            print("------------------------------------------------------------" )
+
+    # Método para mostrar el estado del libro
+    def mostrar_estado(self):
+        estado = "Prestado" if self.prestado else "Disponible"
+        print(f"Título: {self.titulo}")
+        print(f"Autor: {self.autor}")
+        print(f"Año de publicación: {self.anio_publicacion}")
+        print(f"Estado: {estado}")
+        print(f"Biblioteca: {self.biblioteca}")
+        print("------------------------------------------------------------" )
